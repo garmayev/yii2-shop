e8cc2c4e6af34588e0c5a22154bc65f4172fa2bf
Garmayev <garmayev@yandex.ru>
2021-05-24 13:04:21 +0800

    Blank Extension

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace garmayev\shop;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
